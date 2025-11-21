1edadefe67547b03ed5f21c1af6c9bdf87c8da96
AlbertoPazToja <albertopaztoja@gmail.com>
2025-11-21 11:55:13 +0100

    paraninfo

--- UD3/Ejemplos/Cuenta_Bancaria/Conta.java
+++ UD3/Ejemplos/Cuenta_Bancaria/Conta.java
@@ -5,7 +5,23 @@ public class Conta {
     public int idConta;
     public String titular;
     public double saldo;
-
     // Creamos una clase Conta y le creamos sus atributos (públicos)
+
+    public Conta() {
+    } // Hace falta para que el primer objeto Conta c new Conta se pueda usar vacío.
+
+    public Conta(int idConta, String titular, double saldo) {
+
+        this.idConta = idConta;
+        this.titular = titular;
+        this.saldo = saldo;
+
+    }
+
+    public Contaza (int idConta2, String titular2, double saldo2){
+        
+        this.idConta = idConta2;
+
+    }
 
 }
