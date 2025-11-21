2be79e13f8bcd72aa271e148305531663fb44751
AlbertoPazToja <albertopaztoja@gmail.com>
2025-11-21 11:27:01 +0100

    Ejercicio App Conta

---
+++ UD3/Ejemplos/Cuenta_Bancaria/AppConta.java
@@ -0,0 +1,26 @@
+package Ejemplos.Cuenta_Bancaria;
+
+public class AppConta {
+
+    public static void main(String[] args) {
+
+        // Hacemos un main para poder ejecutar
+
+        Conta c = new Conta();
+
+        // Creamos el objeto c a partir de la clase Conta
+
+        c.idConta = 1;
+        c.titular = "Alberto";
+        c.saldo = 6.70;
+
+        // Le asignamos valores a los atributos del objeto c
+
+        System.out.println(c.idConta);
+        System.out.println(c.titular);
+        System.out.println(c.saldo);
+
+        // Mostramos los valores por pantalla
+
+    }
+}
---
+++ UD3/Ejemplos/Cuenta_Bancaria/Conta.java
@@ -0,0 +1,11 @@
+package Ejemplos.Cuenta_Bancaria;
+
+public class Conta {
+
+    public int idConta;
+    public String titular;
+    public double saldo;
+
+    // Creamos una clase Conta y le creamos sus atributos (públicos)
+
+}
