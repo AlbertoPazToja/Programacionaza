749eb273d179e1d8d6b8b19f81bab8251d15ec94
AlbertoPazToja <albertopaztoja@gmail.com>
2025-11-26 10:30:48 +0100

    ahora imc y tal ejercicio persona completo

--- UD3/Ejemplos/Persona/AppPersona.java
+++ UD3/Ejemplos/Persona/AppPersona.java
@@ -4,7 +4,7 @@ public class AppPersona {
 
     public static void main(String[] args) {
 
-        Persona a = new Persona("Anuel AA", 30, "12345432A", 'M', 50.9, 1.10);
+        Persona a = new Persona("Anuel AA", 49, "12345432A", 'F', 50.9, 1.10);
 
         /*
          * a.setNombre("Michael Jackson");
@@ -15,12 +15,33 @@ public class AppPersona {
          * a.setAltura(1.75);
          */
 
-        System.out.println(a.getNombre());
-        System.out.println(a.getEdad());
-        System.out.println(a.getDNi());
-        System.out.println(a.getSexo());
-        System.out.println(a.getPeso());
-        System.out.println(a.getAltura());
+        /*
+         * System.out.println(a.getNombre());
+         * System.out.println(a.getEdad());
+         * System.out.println(a.getDni());
+         * System.out.println(a.getSexo());
+         * System.out.println(a.getPeso());
+         * System.out.println(a.getAltura());
+         */
+
+        a.amosarInformacion();
+
+        System.out.println("-------------------------------");
+
+        Persona b = new Persona();
+
+        b.setNombre("Catalina");
+        b.setEdad(10);
+        b.setDni("45678987E");
+        b.setSexo('M');
+        b.setPeso(48);
+        b.setAltura(1.78);
+
+        b.amosarInformacion();
+
+        // Creamos setters del nuevo objeto, para comprobar las restricciones que
+        // pusimos en los setter, ya que no utilizamos el constructor, donde teníamos
+        // las restricciones antes
 
     }
 
--- UD3/Ejemplos/Persona/Persona.java
+++ UD3/Ejemplos/Persona/Persona.java
@@ -8,7 +8,10 @@ public class Persona {
     private double peso;
     private double altura;
 
+    // Creamos los atributos de la clase Persona, y los ponemos en private
+
     public Persona(String nombre, int edad, String dni, char sexo, double peso, double altura) {
+        // Creamos el constructor, y le asignamos un valor a cada atributo
 
         this.nombre = nombre;
 
@@ -23,6 +26,9 @@ public class Persona {
             System.out.println("Edad incorrecta. Introduce un valor entre 0 y 150");
         }
 
+        // Si esCorrectaEdad == true (la edad está comprendida entre 0 y 150), se le
+        // asigna el valor, y si no se imprime que la edad es incorrecta
+
         this.dni = dni;
 
         if (sexo == 'H' || sexo == 'M') {
@@ -34,6 +40,9 @@ public class Persona {
 
             System.out.println("Solo puede ser H o M");
         }
+
+        // Si el sexo es H o M, se asigna, y si no no, y se imprime que solo puede ser H
+        // y M
 
         this.peso = peso;
 
@@ -47,7 +56,16 @@ public class Persona {
 
     public void setEdad(int edad) {
 
-        this.edad = edad;
+        if (esCorrectaEdad(edad) == true) {
+
+            this.edad = edad;
+
+        }
+
+        else {
+
+            System.out.println("Edad incorrecta. Introduce un valor entre 0 y 150");
+        }
     }
 
     public void setDni(String dni) {
@@ -57,7 +75,15 @@ public class Persona {
 
     public void setSexo(char sexo) {
 
-        this.sexo = sexo;
+        if (sexo == 'H' || sexo == 'M') {
+
+            this.sexo = sexo;
+        }
+
+        else {
+
+            System.out.println("Solo puede ser H o M");
+        }
     }
 
     public void setPeso(double peso) {
@@ -80,7 +106,7 @@ public class Persona {
         return edad;
     }
 
-    public String getDNi() {
+    public String getDni() {
 
         return dni;
     }
@@ -101,6 +127,9 @@ public class Persona {
         return altura;
     }
 
+    // Hacemos todos los setters y getters de cada atributo para poder llamarlos
+    // desde otra clase siendo los atributos privados
+
     private boolean esCorrectaEdad(int edad) {
 
         if (edad >= 0 && edad <= 150) {
@@ -113,5 +142,67 @@ public class Persona {
             return false;
         }
     }
+
+    // Creamos un método que devuelve un boolean, true si la edad es correcta y
+    // false si la edad no es correcta
+
+    public void amosarInformacion() {
+
+        System.out.println("El nombre es: " + this.getNombre());
+        System.out.println("La edad es: " + this.getEdad());
+        System.out.println("El DNI es: " + this.getDni());
+        System.out.println("El sexo es: " + this.getSexo());
+        System.out.println("El peso es: " + this.getPeso() + "kg");
+        System.out.println("La altura es: " + this.getAltura() + " metros");
+
+        if (esMayorDeEdad() == true) {
+
+            System.out.println("Es mayor de edad");
+        }
+
+        if (esMayorDeEdad() == false) {
+
+            System.out.println("Es menor de edad");
+        }
+
+        System.out.println("El índice de masa corporal es: " + this.calcularIMC());
+    }
+
+    // Creamos un método para que muestre todos los datos cuando se llame
+
+    public Persona() {
+    }
+
+    public boolean esMayorDeEdad() {
+
+        if (edad >= 18) {
+
+            return true;
+        }
+
+        else {
+
+            return false;
+        }
+
+    }
+
+    // Creamos un método que es true si la edad es mayor de 18 y si no es false, y
+    // en el método amosarInformacion ponemos que si el método es true que imprima
+    // que sí es mayor de edad y si es false que no
+
+    public double calcularIMC() {
+
+        double imc = 0;
+
+        imc = this.getPeso() / (this.getAltura() * this.getAltura());
+
+        return imc;
+    }
+
+    // Creamos el método calcularIMC, que calcula el índice dividiendo el peso entre
+    // la altura al cuadrado, y devuelve el resultado dentro de la variable imc
+    // (return imc), y luego se imprime el método con el otro método
+    // amosarInformación
 
 }
