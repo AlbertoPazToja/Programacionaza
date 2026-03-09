d13dee4c4fc4c4ee39101c074bdf3a1fb1853016
AlbertoPazToja <albertopaztoja@gmail.com>
2026-03-09 10:31:05 +0100

    Ejercicio matriz baleira completao

--- Eva2/UD5/5_2/Ejemplos/EjercicioMatrizBaleira.java
+++ Eva2/UD5/5_2/Ejemplos/EjercicioMatrizBaleira.java
@@ -1,12 +1,13 @@
 public class EjercicioMatrizBaleira {
 
     public static void main(String[] args) {
-
+        // Creamos a matriz de 3x5 de enteiros
         int[][] matriz = new int[3][5];
 
         int i = 0;
         int j = 0;
 
+        // Percorremos a matriz e gardamos cada elemento e o imprimimos
         for (i = 0; i < matriz.length; i++) {
             for (j = 0; j < matriz[i].length; j++) {
 
