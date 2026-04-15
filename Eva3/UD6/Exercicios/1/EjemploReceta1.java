c2fadf8336a1317def84ad07226b7a5dad77882d
AlbertoPazToja <albertopaztoja@gmail.com>
2026-04-15 09:07:33 +0200

    Ejercicio 6.1 completo faltan comentarios

--- Eva3/UD6/Exercicios/1/EjemploReceta1.java
+++ Eva3/UD6/Exercicios/1/EjemploReceta1.java
@@ -1,13 +1,3 @@
 public class EjemploReceta1 {
 
-    public static void main(String[] args) {
-
-        Ingredientes i1 = new Ingredientes("Patatas", 200, "gramos");
-        Ingredientes i2 = new Ingredientes("Huevos", 2, "unidades");
-        Receita tortillaDePatatas = new Receita();
-
-        System.out.println("Receta para hacer tortilla de patatas: " + tortillaDePatatas);
-
-    }
-
 }
--- Eva3/UD6/Exercicios/1/Receita.java
+++ Eva3/UD6/Exercicios/1/Receita.java
@@ -2,22 +2,51 @@ import java.util.ArrayList;
 
 public class Receita {
 
-    ArrayList<Ingredientes> receita = new ArrayList<>();
+    private String nome;
 
-    public Receita() {
+    ArrayList<Ingredientes> ingredientesReceta = new ArrayList<>();
 
+    public Receita(String nome, ArrayList<Ingredientes> ingredientesReceta) {
+        this.nome = nome;
+        this.ingredientesReceta = ingredientesReceta;
     }
 
-    public Receita(ArrayList<Ingredientes> receita) {
-        this.receita = receita;
+    public String getNome() {
+        return nome;
     }
 
-    public void setReceita(ArrayList<Ingredientes> receita) {
-        this.receita = receita;
+    public void setNome(String nome) {
+        this.nome = nome;
     }
 
-    public ArrayList<Ingredientes> getReceita() {
-        return receita;
+    public ArrayList<Ingredientes> getIngredientesReceta() {
+        return ingredientesReceta;
+    }
+
+    public void setIngredientesReceta(ArrayList<Ingredientes> ingredientesReceta) {
+        this.ingredientesReceta = ingredientesReceta;
+    }
+
+    public static void main(String[] args) {
+
+        Ingredientes i1Tortilla = new Ingredientes("Patatas", 200, "gramos");
+        Ingredientes i2Tortilla = new Ingredientes("Huevos", 2, "unidades");
+
+        ArrayList<Ingredientes> ingredientesTortilla = new ArrayList<>();
+        ingredientesTortilla.add(i1Tortilla);
+        ingredientesTortilla.add(i2Tortilla);
+
+        Receita tortilla = new Receita("Tortilla de patatas", ingredientesTortilla);
+
+        System.out.println("Receta para " + tortilla.nome + ", ingredientes: ");
+
+        for (Ingredientes ingredienteActual : ingredientesTortilla) {
+
+            System.out.println(ingredienteActual.getCantidade() + " " + ingredienteActual.getUnidade() + " de "
+                    + ingredienteActual.getNome());
+
+        }
+
     }
 
 }
