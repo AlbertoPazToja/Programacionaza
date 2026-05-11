b23e50333f66652cfc33be3fddd6b4b3e34a3510
AlbertoPazToja <albertopaztoja@gmail.com>
2026-05-11 08:48:38 +0200

    ver directorio 2

--- Eva3/UD7/7.2/Exemplos/VerDirectoriosExemplo/VerDir2.java
+++ Eva3/UD7/7.2/Exemplos/VerDirectoriosExemplo/VerDir2.java
@@ -4,7 +4,7 @@ public class VerDir2 {
     public static void main(String[] args) {
         String dir = ".";
         File f = new File(dir); // Creamos o File que representa ao directorio
-        String[] archivos = f.list(); // Lístase o directorio
+        File[] archivos = f.listFiles(); // Lístase o directorio
         System.out.printf("Ficheros en el directorio actual: %d %n",
                 archivos.length);
         for (int i = 0; i < archivos.length; i++) {
